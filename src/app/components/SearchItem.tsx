d966d4dbbd92af7ae2625a6064e99c9d0ed0f7d8
ying <leftleft0813@gmail.com>
2024-09-14 01:43:32 +0800

    style: 調整搜尋結果樣式

--- src/app/components/SearchItem.tsx
+++ src/app/components/SearchItem.tsx
@@ -8,20 +8,21 @@ interface SearchItemProps {
 const SearchItem: React.FC<SearchItemProps> = ({ active }) => {
   return (
     <div
-      className={`h-60 w-[720px] flex items-center gap-4 border border-lightblue rounded p-[15px] mb-[15px] ${
-        active ? "bg-blue-200 shadow-3xl" : ""
+      className={`flex items-center gap-4 border border-lightblue rounded p-4 mb-4 ${
+        active ? "bg-blue-200 shadow-lg" : ""
       }`}
     >
-      <Image
-        className="h-[210px] w-[200px] object-cover rounded"
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/347072190.webp?k=74cb5ec7f0ef6a6b424dca16d22b2e0b62c5438fbeef2e9f56bed64167dddbad&o=&s=1"
-        alt=""
-        width={150}
-        height={50}
-      />
-      <div className="w-[505px] h-[210px]">
+      <div className="w-1/4 aspect-square relative rounded overflow-hidden">
+        <Image
+          className="object-cover"
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/347072190.webp?k=74cb5ec7f0ef6a6b424dca16d22b2e0b62c5438fbeef2e9f56bed64167dddbad&o=&s=1"
+          alt=""
+          layout="fill"
+        />
+      </div>
+      <div className="w-3/4 flex flex-col justify-between">
         <div className="flex justify-between">
-          <h2 className="text-xl w-[300px]">台南微醺文旅|老宅古城|漫遊體驗</h2>
+          <h2 className="text-xl w-1/2">台南微醺文旅 | 老宅古城 | 漫遊體驗</h2>
           <div className="flex gap-[5px] text-right">
             傑出
             <br />
@@ -32,36 +33,32 @@ const SearchItem: React.FC<SearchItemProps> = ({ active }) => {
           </div>
         </div>
         <div className="mt-2">
-          <span className="text-blue-500 border-b border-blue-500 text-xs mr-[5px]">
+          <span className="text-blue-500 border-b border-blue-500 text-xs mr-2">
             中西區 台南 400公尺遠
           </span>
-          <span className="inline-flex items-center justify-center text-white text-xs font-light p-[5px] bg-blue-500">
+          <span className="inline-flex items-center justify-center text-white text-xs font-light p-1 bg-blue-500">
             免費專機接送
           </span>
 
-          <div className="mt-[10px] flex justify-between items-end">
-            <div className="border-l-[3px] border-lightgray p-[15px_5px] text-xs">
+          <div className="mt-4 flex justify-between items-end">
+            <div className="border-l-4 border-gray-300 pl-2 text-xs">
               <div>
                 <b>標準單人房－附共用衛浴</b>
+                <br />
                 <b>一張單人床</b>
               </div>
-              <div className="mt-[10px] text-green-600">
-                <b>免費取消</b>
+              <div className="mt-2 text-green-600">
+                <b>✔️ 免費取消</b>
                 <b>立即搶下優惠價－可取消</b>
-                <br />
                 <br />
                 <b className="text-red-600">此價格的客房在本站僅剩 1 間</b>
               </div>
             </div>
-            <div className="text-xs">
-              <span className="flex justify-end">五晚、1位</span>
-              <span className="flex justify-end text-xl font-medium">
-                TWD 4534
-              </span>
-              <span className="flex justify-end text-gray-500">
-                含稅費與其他費用
-              </span>
-              <button className="mt-[10px] w-[200px] text-white text-lg py-[5px] px-[10px] bg-[rgb(255,142,4)] rounded hover:bg-[rgb(206,114,1)] cursor-pointer">
+            <div className="text-xs flex flex-col items-end">
+              <span>五晚、1位</span>
+              <span className="text-xl font-medium">TWD 4534</span>
+              <span className="text-gray-500">含稅費與其他費用</span>
+              <button className="mt-2 w-full text-white text-lg py-2 px-4 bg-blue-500 rounded hover:bg-blue-400 transition-colors duration-300">
                 查看客房供應情況
               </button>
             </div>
