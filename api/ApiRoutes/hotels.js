e8325234911a08b33a318f98ebfd2bbc3cdc12ef
ying <leftleft0813@gmail.com>
2024-11-15 23:03:49 +0800

    fix: 將重複內容移除

--- api/ApiRoutes/hotels.js
+++ api/ApiRoutes/hotels.js
@@ -12,33 +12,8 @@ const router = express.Router();
 // 創建第一筆資料
 router.post("/", createHotel);
 
-// 取得所有資料
-router.get("/", getHotel);
-
-// 根據 id 取得特定資料
-router.get("/:id", async (req, res, next) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    if (hotel) {
-      res.status(200).json(hotel);
-    } else {
-      res.status(404).json({ message: "Hotel not found" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
 // 找尋第一筆資料
-router.get("/find/:id", async (req, res, next) => {
-  const id = req.params.id;
-  try {
-    const getHotel = await Hotel.findById(id);
-    res.status(200).json(getHotel);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/find/:id", getHotel);
 
 // 修改第一筆資料
 router.put("/:id", updatedHotel);
