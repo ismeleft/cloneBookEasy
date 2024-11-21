b5af30140229c52ad0afec5ecf0528c5a553ba19
Lulu <s9341729@gmail.com>
2024-11-21 22:37:54 +0800

    fix. 將 createHotel 的 status code 更新為 201

--- api/RoutesController/hotels.js
+++ api/RoutesController/hotels.js
@@ -5,7 +5,7 @@ const createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
   try {
     const saveHotel = await newHotel.save();
-    res.status(200).json(saveHotel);
+    res.status(201).json(saveHotel);
   } catch (error) {
     next(errorMessage(400, "無法創建飯店資料，請檢查輸入格式是否正確", error));
   }
