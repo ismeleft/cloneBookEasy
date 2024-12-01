acaf6eb1ee361ae73602851a797f11d8ad8a17e7
ying <leftleft0813@gmail.com>
2024-12-01 14:10:01 +0800

    feat: 建立註冊登入流程 api

--- api/ApiRoutes/auth.js
+++ api/ApiRoutes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { register, login } from "../RoutesController/auth.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("這邊是 AuthApi End points 連接點");
 });
+
+router.post("/register", register);
+
+router.post("/login", login);
 
 export default router;
